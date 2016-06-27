7c0810bf424cba4dd93cc1ce654a9efef6d00374
gonzant93 <gonzant93@gmail.com>
2016-06-27 11:38:15 -0700

    Signed-off-by: gonzant93 <gonzant93@gmail.com>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on June 20, 2016, 6:51 PM
+ * Purpose:  First Program
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    bool x,y;
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    //Heading
+    cout<<"The Truth Table"<<endl;
+    cout<<"X Y !X !Y X||Y X&&Y X^Y X^Y^Y X^Y^X !(X||Y) !X&&!Y ";
+    cout<<"!(X&&Y)!X||!Y "
+    
+            
+    //First Row
+    x=true; y=true;
+    
+    //Exit Stage Right!
+   
+    return 0;
+}
