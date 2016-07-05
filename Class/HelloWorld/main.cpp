8300d55bffa8da9cc93218528044b282f21dcb9b
gonzant93 <gonzant93@gmail.com>
2016-07-05 12:23:44 -0700

    Signed-off-by: gonzant93 <gonzant93@gmail.com>

---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on June 20, 2016, 6:51 PM
+ * Purpose:  First Program
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    std::cout<<"Hello World"<<std::endl;
+    
+    //Exit Stage Right!
+    return 0;
+}
---
+++ Class/Template/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on June 20, 2016, 6:51 PM
+ * Purpose:  Template
+ */
+
+//System Libraries
+#include <iostream>  //Input/Output Library
+using namespace std; //Namespace of the System Libraries
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Declare Variables
+    
+    //Input Data
+    
+    //Process the Data
+    
+    //Output the processed Data
+    
+    //Exit Stage Right!
+    return 0;
+}
