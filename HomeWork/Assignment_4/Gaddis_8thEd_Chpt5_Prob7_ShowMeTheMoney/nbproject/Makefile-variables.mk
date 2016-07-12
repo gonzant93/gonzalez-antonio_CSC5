b2a87ac8f7e96e4ee60f6b5b4c45cd911504367f
gonzant93 <gonzant93@gmail.com>
2016-07-11 22:35:49 -0700

    Hw

--- HomeWork/Assignment_4/Gaddis_8thEd_Chpt5_Prob7_ShowMeTheMoney/nbproject/Makefile-variables.mk
+++ HomeWork/Assignment_4/Gaddis_8thEd_Chpt5_Prob7_ShowMeTheMoney/nbproject/Makefile-variables.mk
@@ -7,13 +7,13 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin_4.x-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_PLATFORM_Debug=GNU-MacOSX
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-MacOSX
 CND_ARTIFACT_NAME_Debug=gaddis_8thed_chpt5_prob7_showmethemoney
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/gaddis_8thed_chpt5_prob7_showmethemoney
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-MacOSX/gaddis_8thed_chpt5_prob7_showmethemoney
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-MacOSX/package
 CND_PACKAGE_NAME_Debug=gaddis8thedchpt5prob7showmethemoney.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/gaddis8thedchpt5prob7showmethemoney.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-MacOSX/package/gaddis8thedchpt5prob7showmethemoney.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
