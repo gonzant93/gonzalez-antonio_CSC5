b0c3ad774642305d0f8539160c5446cada60468b
gonzant93 <gonzant93@gmail.com>
2016-07-20 11:34:12 -0700

    Signed-off-by: gonzant93 <gonzant93@gmail.com>

--- Class/Search/nbproject/Makefile-impl.mk
+++ Class/Search/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=HelloWorld
+PROJECTNAME=Search
 
 # Active Configuration
 DEFAULTCONF=Debug
--- Class/Search/nbproject/Makefile-variables.mk
+++ Class/Search/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=helloworld
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/helloworld
+CND_ARTIFACT_NAME_Debug=search
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/search
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=helloworld.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/helloworld.tar
+CND_PACKAGE_NAME_Debug=search.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/search.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=helloworld
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/helloworld
+CND_ARTIFACT_NAME_Release=search
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/search
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=helloworld.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/helloworld.tar
+CND_PACKAGE_NAME_Release=search.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/search.tar
 #
 # include compiler specific variables
 #
